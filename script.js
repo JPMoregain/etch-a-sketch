0fcd4a43ce9389d9216d85f16cfbb4f0540d4ad6
JP Moregain <j.p.moregain15@gmail.com>
2022-04-12 11:38:59 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+var container = document.getElementById("grid");
+var cell = document.createElement("div");
+container.appendChild(cell).className = "cell";
+
+var cell2 = document.createElement("div");
+container.appendChild(cell2).className = "cell";
